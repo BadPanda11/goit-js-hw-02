84c24691d070457028dc7ec0c0661061d376a373
Rodion Mokan <mokan11r@gmail.com>
2020-04-28 18:42:52 +0300

    1

--- js/task3.js
+++ js/task3.js
@@ -1,13 +1,15 @@
 let findLongestWord = function (string) {
   let massive = string.split(' ');
-  let longestWord = 0;
+  console.log(massive);
+  let word = massive[0].length;
+  let longestWord;
 
   for (let i = 1; i < massive.length; i += 1) {
-    if (massive[i].length > longestWord) {
+    if (massive[i].length > word) {
       longestWord = massive[i];
+      return longestWord;
     }
   }
-  return longestWord;
 };
 
 console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'

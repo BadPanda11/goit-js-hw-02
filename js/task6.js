5b5762d4e6d608c8d1cfc05f4e24ef4c58ab8216
Rodion Mokan <mokan11r@gmail.com>
2020-04-29 17:01:23 +0300

    done

---
+++ js/task6.js
@@ -0,0 +1,22 @@
+let total = 0;
+const numbers = [];
+do {
+  let input = prompt('Введи число');
+  if (input === null) {
+    for (const number of numbers) {
+      total += number;
+    }
+    console.log(`общая сумма чисел равна ${total}`);
+
+    break;
+  }
+  input = Number(input);
+  const notANumber = Number.isNaN(input);
+
+  if (notANumber) {
+    console.log(notANumber);
+    continue;
+  }
+  numbers.push(input);
+  console.log(numbers);
+} while (true);

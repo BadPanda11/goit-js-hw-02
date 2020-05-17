7ef68fae41abc562758a47e4d98a3863a007c543
Rodion Mokan <mokan11r@gmail.com>
2020-05-17 08:47:43 +0300

    change indexOf

--- js/task1.js
+++ js/task1.js
@@ -1,8 +1,11 @@
 const logItems = function (array) {
-  let result;
+  
   for (let i = 0; i < array.length; i += 1) {
-    console.log(`${i + 1} - ${array[i]}`);
+    
+    console.log(`${array.indexOf(array[i]) + 1} - ${array[i]}`);
   }
 };
 logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
 logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+const string = 'javascript';
+
